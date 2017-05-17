897371960aa52fd0a0fd544b28753e8e6208ea56
Vitaly Kuzenkov <vitalykuzenkov@gmail.com>
2017-05-17 13:30:23 +1000

    UI-update

--- Assignment 2 Bubble Game/ButtonCustomView.swift
+++ Assignment 2 Bubble Game/ButtonCustomView.swift
@@ -16,10 +16,10 @@ class ButtonCustoomView: UIButton {
 		
 		//defines the parameters for shadows
 		layer.shadowColor = UIColor(displayP3Red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 1.0).cgColor
-		layer.shadowOpacity = 1.0
-		layer.shadowRadius = 5.0
+		layer.shadowOpacity = 1
+		layer.shadowRadius = 2.0
 		layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-		layer.cornerRadius = 2.0
+		layer.cornerRadius = 5.0
 		
 	}
 	
